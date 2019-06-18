1a069df2ee695820feea4d29c9c84da6d2fe33ef
huggeri <katq228@gmail.com>
2019-06-18 18:47:42 +0700

    header, first section, slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,2 @@
 $(document).ready(function(){
-  if($(window).width() <= 1190) {
-    $('.slick-tables-script').attr('src', 'js/slick-tables.js')
-  }
 });
---
+++ src/js/slick-images.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.upper-section').slick( {
+    infinite: false,
+    dots: true,
+    dotsClass: 'my-dots',
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
