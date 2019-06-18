9eba8eef9e34cacf8b62032da5609faac63fb71b
huggeri <katq228@gmail.com>
2019-06-19 01:55:14 +0700

    services + main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  // изменение текста при наведении в услугах и назад, при убирании мыши
+  var hoverText = 'Здесь будет короткое описание<br>о данной услуге';
+  var hoverTextButton = 'от 500 р.';
+  var serviceText, serviceTextButton;
+
+  $('.services__item').mouseenter(function() {
+    serviceText = $('#service-description', this).html();
+    serviceTextButton = $('#service-button', this).html();
+    $('#service-description', this).html(hoverText);
+    $('#service-button', this).html(hoverTextButton);
+  });
+
+  $('.services__item').mouseleave(function() {
+    $('#service-description', this).html(serviceText);
+    $('#service-button', this).html(serviceTextButton);
+  });
 });
