c8893933fff01cccd5e7b0a53b0557b9bb2a33ee
huggeri <katq228@gmail.com>
2019-06-19 15:49:25 +0700

    ещё две секции

--- src/js/slick-my-script.js
+++ src/js/slick-my-script.js
@@ -7,4 +7,13 @@ $(document).ready(function() {
     slidesToShow: 1,
     slidesToScroll: 1
   });
+
+  $('.reviews-block').slick( {
+    infinite: false,
+    dots: true,
+    dotsClass: 'my-dots',
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 2
+  });
 });
