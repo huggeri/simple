6cc26e9ec28c848e7385185655ed1e5b00f4f754
huggeri <katq228@gmail.com>
2019-06-21 21:50:59 +0700

    navbar.js debag

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -4,20 +4,24 @@ $(document).ready(function() {
   // при изменении размера - бургер меню или обычное меню
   $(window).resize(showHideBurgerMenu);
   // при клике на бургер раскрывается меню
-  $('#burger').click(function() {
-    if($('.navbar__menu').hasClass('hidden')) {
-      $('.navbar__menu').removeClass('hidden');
-    }
-    else {
-      $('.navbar__menu').addClass('hidden');
-    }
-  });
-  // если убрали мышь с менб - оно исчезает
-  $('.navbar__menu').mouseleave(function() {
-    if(!$('.navbar__menu').hasClass('hidden')) {
-      $('.navbar__menu').addClass('hidden');
-    }
-  });
+    $('#burger').click(function() {
+      if($(window).width() <= 1199) {
+        if($('.navbar__menu').hasClass('hidden')) {
+          $('.navbar__menu').removeClass('hidden');
+        }
+        else {
+          $('.navbar__menu').addClass('hidden');
+        }
+      }
+    });
+    // если убрали мышь с менб - оно исчезает
+    $('.navbar__menu').mouseleave(function() {
+      if($(window).width() <= 1199) {
+        if(!$('.navbar__menu').hasClass('hidden')) {
+          $('.navbar__menu').addClass('hidden');
+        }
+      }
+    });
 });
 
 function showHideBurgerMenu() {
